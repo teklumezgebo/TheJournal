ac5a244b55994329e4a6079adb02e974cf7e6afe
teklumezgebo <mezge002@umn.edu>
2024-03-10 20:38:59 -0500

    Connected MongoDB database to application

--- server/app.py
+++ server/app.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from pymongo import MongoClient
 from flask_restful import Api, Resource
+from config import password
 
 app = Flask(__name__)
+
+cluster = MongoClient(f"mongodb+srv://teklumezgebo:{password}@thejournal.fiahiep.mongodb.net/?retryWrites=true&w=majority&appName=TheJournal")
+
+db = cluster["TheJournal"]
+
+users = db["Users"]
+
 
 if __name__ == '__main__':
     app.run(port=5555,  debug=True)
