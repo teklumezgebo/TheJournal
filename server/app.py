563c64b320fe6e6f6c308d37dfb5039fb025ffca
teklumezgebo <mezge002@umn.edu>
2024-03-10 19:39:44 -0500

    flask-restful package installed

--- server/app.py
+++ server/app.py
@@ -1,1 +1,8 @@
 from flask import Flask
+from pymongo import MongoClient
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(port=5555,  debug=True)
