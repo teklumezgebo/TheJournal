f2ddb77f0a7e81eb3be35cc99885a6eaf4ffc16f
teklumezgebo <mezge002@umn.edu>
2024-03-11 16:57:05 -0500

    added seed data to seed.py file

--- server/seed.py
+++ server/seed.py
@@ -1,8 +1,27 @@
 from pymongo import MongoClient
 from config import database_user_password
+from faker import Faker
 
 cluster = MongoClient(f"mongodb+srv://teklumezgebo:{database_user_password}@thejournal.fiahiep.mongodb.net/?retryWrites=true&w=majority&appName=TheJournal")
 
 db = cluster["TheJournal"]
 
 users = db["Users"]
+
+fake = Faker()
+
+i = 0
+
+print("Creating Seed Data...")
+
+while i < 50:
+    users.insert_one({
+        "first_name": fake.first_name(),
+        "last_name": fake.last_name(),
+        "email": fake.email(),
+        "journals": []
+    })
+    
+    i += 1
+
+print("Seeding Complete!")
