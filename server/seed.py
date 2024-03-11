61d6503f533f2e4c0310b69e9299e9f07acc0052
teklumezgebo <mezge002@umn.edu>
2024-03-11 18:14:56 -0500

    seed.py imports changed

--- server/seed.py
+++ server/seed.py
@@ -1,12 +1,5 @@
-from pymongo import MongoClient
-from config import database_user_password
+from app import users_collection
 from faker import Faker
-
-cluster = MongoClient(f"mongodb+srv://teklumezgebo:{database_user_password}@thejournal.fiahiep.mongodb.net/?retryWrites=true&w=majority&appName=TheJournal")
-
-db = cluster["TheJournal"]
-
-users = db["Users"]
 
 fake = Faker()
 
@@ -15,7 +8,8 @@ i = 0
 print("Creating Seed Data...")
 
 while i < 50:
-    users.insert_one({
+    
+    users_collection.insert_one({
         "first_name": fake.first_name(),
         "last_name": fake.last_name(),
         "email": fake.email(),
