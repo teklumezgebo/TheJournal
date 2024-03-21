fcc8abbbb1a9abf1fa5354e87ce787f8478f3730
teklumezgebo <mezge002@umn.edu>
2024-03-20 19:44:23 -0500

    client file structure updated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import Landing from './pages/Landing';
+import './assets/App.css';
 
 function App() {
   return (
     <div>
-
+      <Landing /> 
     </div>
   );
 }
